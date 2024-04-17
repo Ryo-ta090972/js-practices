6363330b75cf524e914964e7bee4b7978d9adf33
ryo <ta090972@icloud.com>
2024-04-17 09:58:03 +0900

    prettierによる修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,19 +1,20 @@
 for (let cnt = 1; cnt <= 20; cnt++) {
   switch (true) {
     case cnt % 3 === 0 && cnt % 5 === 0:
-      whitePrint('FizzBuzz');
+      whitePrint("FizzBuzz");
       break;
     case cnt % 3 === 0:
-      whitePrint('Fizz');
+      whitePrint("Fizz");
       break;
     case cnt % 5 === 0:
-      whitePrint('Buzz');
+      whitePrint("Buzz");
       break;
     default:
       whitePrint(cnt);
   }
 }
 
-function whitePrint(text){
-  console.log('\x1b[37m' + text);
+function whitePrint(text) {
+  console.log("\x1b[37m" + text);
 }
+
