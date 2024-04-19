07cabd90aaa165c94bdf7179f70ee09de3964141
ryo <ta090972@icloud.com>
2024-04-19 09:45:11 +0900

    当月の日付を出力

---
+++ 02.calendar/calendar.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import dayjs from "dayjs";
+import ja from "dayjs/locale/ja.js";
+dayjs.locale(ja);
+
+const now = dayjs();
+const startDate = now.startOf("month");
+const endDate = now.endOf("month").date();
+const startDay = now.startOf("month").day();
+
+for (let i = 0; i < startDay; i++) {
+  process.stdout.write("  ");
+}
+
+for (let i = 0; i < endDate; i++) {
+  if (startDate.add(i, "d").day() === 6) {
+    process.stdout.write(String(startDate.add(i, "d").date()).padStart(3, " "));
+    console.log("");
+  } else if (startDate.add(i, "d").day() === 0) {
+    process.stdout.write(String(startDate.add(i, "d").date()).padStart(2, " "));
+  } else {
+    process.stdout.write(String(startDate.add(i, "d").date()).padStart(3, " "));
+  }
+}
+
+console.log("");
