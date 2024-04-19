4eb38fcb3b02706a8aae94e0803a223d76f013cd
ryo <ta090972@icloud.com>
2024-04-19 15:28:20 +0900

    リファクタリング {  - メソッド抽出：printDate  - 不要な変数を削除：startDate }

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -9,10 +9,11 @@ const argv = minimist(process.argv.slice(2));
 const now = dayjs();
 const month = Object.keys(argv).includes("m") ? argv.m - 1 : now.month();
 const year = Object.keys(argv).includes("y") ? argv.y : now.year();
-const targetDate = dayjs().year(year).month(month);
-const startDate = targetDate.startOf("month");
+const targetDate = dayjs().year(year).month(month).date(1);
 const endDate = targetDate.endOf("month").date();
 const startDay = targetDate.startOf("month").day();
+const printDate = (date) =>
+  process.stdout.write(String(date.date()).padStart(2, " "));
 
 console.log(`      ${month + 1}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
@@ -21,15 +22,14 @@ for (let i = 0; i < startDay; i++) {
   process.stdout.write("   ");
 }
 
-for (let i = 0; i < endDate; i++) {
-  if (startDate.add(i, "d").day() === 6) {
-    process.stdout.write(String(startDate.add(i, "d").date()).padStart(2, " "));
+for (let count = 0; count < endDate; count++) {
+  const date = targetDate.add(count, "d");
+
+  if (date.day() === 6) {
+    printDate(date);
     console.log();
-  } else if (startDate.add(i, "d").day() === 0) {
-    process.stdout.write(String(startDate.add(i, "d").date()).padStart(2, " "));
-    process.stdout.write(" ");
   } else {
-    process.stdout.write(String(startDate.add(i, "d").date()).padStart(2, " "));
+    printDate(date);
     process.stdout.write(" ");
   }
 }
