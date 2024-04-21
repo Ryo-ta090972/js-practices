7ae1645f6d2d0ebcbcbcae744f8df9537067323e
ryo <ta090972@icloud.com>
2024-04-21 13:29:31 +0900

    print関数を削除

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env node
 
-function print(text){
-  console.log(String(text));
-}
-
 for (let count = 1; count <= 20; count++) {
-  if (count % 3 === 0 && count % 5 === 0){
-    print("FizzBuzz");
-  } else if (count % 3 === 0){
-    print("Fizz");
-  } else if (count % 5 === 0){
-    print("Buzz");
+  if (count % 3 === 0 && count % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (count % 3 === 0) {
+    console.log("Fizz");
+  } else if (count % 5 === 0) {
+    console.log("Buzz");
   } else {
-    print(count);
+    console.log(String(count));
   }
 }
