db878be6654a3d0ef642d23799a80a41cdc3b2cd
ryo <ta090972@icloud.com>
2024-04-25 13:34:40 +0900

    不要な出力を削除、捕捉するエラーを設定：async_await.js

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import { resolve } from "path";
 import sqlite3 from "sqlite3";
 import timers from "timers/promises";
 
@@ -29,54 +28,56 @@ function all(db, sql, params = []) {
 }
 
 async function sample(db) {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("データベース生成完了");
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
 
-    await run(db, "INSERT INTO books(title) values(?)", [
-      "プロを目指す人のためのRuby入門",
-    ]);
-    console.log("データ挿入完了");
+  const result = await run(db, "INSERT INTO books(title) values(?)", [
+    "プロを目指す人のためのRuby入門",
+  ]);
+  console.log("追加したID:", result.lastID);
 
-    const rows = await all(db, "SELECT * FROM books");
-    console.log("データ取得完了：", rows);
+  const rows = await all(db, "SELECT * FROM books");
+  console.log("取得したデータ：", rows);
 
-    await run(db, "DROP TABLE books");
-    console.log("データ削除完了");
-  } catch (error) {
-    console.log("エラー：", error);
-  }
+  await run(db, "DROP TABLE books");
 }
 
-await timers.setTimeout(100);
-
 async function sample2(db) {
-  try {
-    await run(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("データベース生成完了");
+  await run(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
 
+  try {
     await run(db, "INSERT INTO books(content) values(?)", [
       "Rubyを知れば、Railsはもっと楽しくなる",
     ]);
-    console.log("データ挿入完了");
-
-    const rows = await all(db, "SELECT * FROM books");
-    console.log("データ取得完了：", rows);
-
-    await run(db, "DROP TABLE books");
-    console.log("データ削除完了");
   } catch (error) {
-    console.log("エラー：", error);
+    if (error.code === "SQLITE_ERROR") {
+      console.log("発生したエラー:", error.message);
+    }
   }
+
+  try {
+    const rows = await all(db, "SELECT * FROM games");
+    console.log("取得したデータ：", rows);
+  } catch (error) {
+    if (error.code === "SQLITE_ERROR") {
+      console.log("発生したエラー:", error.message);
+    }
+  }
+
+  await run(db, "DROP TABLE books");
 }
 
 const db = new sqlite3.Database(":memory:");
 
+// エラー無し
 sample(db);
+
+await timers.setTimeout(100);
+
+// エラーあり
 sample2(db);
