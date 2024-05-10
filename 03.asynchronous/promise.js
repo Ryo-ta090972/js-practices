0e2ba520ab58df5773652e8de5366b43efe0095d
ryo <ta090972@icloud.com>
2024-05-10 17:49:25 +0900

    エラーが発生した時はconsole.logを実行しないようにした

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -2,9 +2,32 @@
 
 import sqlite3 from "sqlite3";
 import timers from "timers/promises";
-import { run, all } from "./common_functions.js";
 
 const database = new sqlite3.Database(":memory:");
+
+function run(db, sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(this);
+      }
+    });
+  });
+}
+
+function all(db, sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (error, rows) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+}
 
 // エラー無し
 run(
@@ -12,7 +35,7 @@ run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 )
   .then(() =>
-    run(database, "INSERT INTO books(title) VALUES(?)", [
+    run(database, "INSERT INTO books (title) VALUES(?)", [
       "プロを目指す人のためのRuby入門",
     ]),
   )
@@ -39,16 +62,18 @@ run(
   )
   .catch((error) => {
     console.error("発生したエラー：", error.message);
+    return error;
   })
   .then((result) => {
-    console.log("追加したID:", result.lastID);
+    if (!(result instanceof Error)) console.log("追加したID:", result.lastID);
     return all(database, "SELECT * FROM games");
   })
   .catch((error) => {
     console.error("発生したエラー：", error.message);
+    return error;
   })
   .then((rows) => {
-    console.log("取得したデータ：", rows);
+    if (!(rows instanceof Error)) console.log("取得したデータ：", rows);
     return run(database, "DROP TABLE books");
   })
   .catch((error) => {
