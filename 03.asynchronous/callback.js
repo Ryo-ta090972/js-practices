525c2d7e91c0a0f8805bbeaafa72f4bdb103f4b0
ryo <ta090972@icloud.com>
2024-05-12 22:02:28 +0900

    重複した条件判定式をDRY

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -5,6 +5,10 @@ import timers from "timers/promises";
 
 const database = new sqlite3.Database(":memory:");
 
+function checkErrorOfSqlite(object) {
+  return object instanceof Error && object.code === "SQLITE_ERROR";
+}
+
 // エラー無し
 database.run(
   "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
@@ -33,13 +37,13 @@ database.run(
       "INSERT INTO books (content) VALUES(?)",
       ["Rubyを知れば、Railsはもっと楽しくなる"],
       function (result) {
-        if (result instanceof Error && result.code === "SQLITE_ERROR") {
+        if (checkErrorOfSqlite(result)) {
           console.error("発生したエラー:", result.message);
         } else {
           console.log("追加したID:", this.lastID);
         }
         database.all("SELECT * FROM games", (error, rows) => {
-          if (error instanceof Error && error.code === "SQLITE_ERROR") {
+          if (checkErrorOfSqlite(error)) {
             console.error("発生したエラー:", error.message);
           } else {
             console.log("取得したデータ:", rows);
