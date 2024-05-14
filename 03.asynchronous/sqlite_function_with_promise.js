8a778cec439d0035c4e8ea4fb19081ec460c680b
ryo <ta090972@icloud.com>
2024-05-14 13:36:51 +0900

    別の関数でエラーハンドリングを行うため、promise関数内のエラー条件判定式を削除

--- 03.asynchronous/sqlite_function_with_promise.js
+++ 03.asynchronous/sqlite_function_with_promise.js
@@ -1,10 +1,8 @@
-import { checkErrorOfSqlite } from "./check_error.js";
-
 export function run(database, sql, params = []) {
   return new Promise((resolve, reject) => {
-    database.run(sql, params, function (result) {
-      if (checkErrorOfSqlite(result)) {
-        reject(result);
+    database.run(sql, params, function (error) {
+      if (error) {
+        reject(error);
       } else {
         resolve(this);
       }
@@ -15,7 +13,7 @@ export function run(database, sql, params = []) {
 export function all(database, sql, params = []) {
   return new Promise((resolve, reject) => {
     database.all(sql, params, (error, rows) => {
-      if (checkErrorOfSqlite(error)) {
+      if (error) {
         reject(error);
       } else {
         resolve(rows);
