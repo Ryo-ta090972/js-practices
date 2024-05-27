bf07c9fd85786806dfe55491cb1c5bd8a9d81f47
ryo <ta090972@icloud.com>
2024-05-27 12:25:40 +0900

    関数を削除し、Top-level awaitに変更

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -9,61 +9,49 @@ import { handleDatabaseError } from "./handle_error.js";
 
 const database = new sqlite3.Database(":memory:");
 
-async function databaseWithNoExistingError(database) {
-  await runWithPromise(
-    database,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
+// エラー無し
+await runWithPromise(
+  database,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
+const statement = await runWithPromise(
+  database,
+  "INSERT INTO books (title) VALUES (?)",
+  ["プロを目指す人のためのRuby入門"],
+);
+console.log("追加したID:", statement.lastID);
+
+const rowsOfBooksTable = await allWithPromise(database, "SELECT * FROM books");
+console.log("取得したデータ：", rowsOfBooksTable);
+
+await runWithPromise(database, "DROP TABLE books");
+
+// エラーあり
+await runWithPromise(
+  database,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+
+try {
   const statement = await runWithPromise(
     database,
-    "INSERT INTO books (title) VALUES (?)",
-    ["プロを目指す人のためのRuby入門"],
+    "INSERT INTO books (content) VALUES (?)",
+    ["Rubyを知れば、Railsはもっと楽しくなる"],
   );
   console.log("追加したID:", statement.lastID);
+} catch (error) {
+  handleDatabaseError(error);
+}
 
-  const rowsOfBooksTable = await allWithPromise(
+try {
+  const rowsOfGamesTable = await allWithPromise(
     database,
-    "SELECT * FROM books",
+    "SELECT * FROM games",
   );
-  console.log("取得したデータ：", rowsOfBooksTable);
-
-  await runWithPromise(database, "DROP TABLE books");
+  console.log("取得したデータ：", rowsOfGamesTable);
+} catch (error) {
+  handleDatabaseError(error);
 }
 
-async function databaseWithExistingError(database) {
-  await runWithPromise(
-    database,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-
-  try {
-    const statement = await runWithPromise(
-      database,
-      "INSERT INTO books (content) VALUES (?)",
-      ["Rubyを知れば、Railsはもっと楽しくなる"],
-    );
-    console.log("追加したID:", statement.lastID);
-  } catch (error) {
-    handleDatabaseError(error);
-  }
-
-  try {
-    const rowsOfGamesTable = await allWithPromise(
-      database,
-      "SELECT * FROM games",
-    );
-    console.log("取得したデータ：", rowsOfGamesTable);
-  } catch (error) {
-    handleDatabaseError(error);
-  }
-
-  await runWithPromise(database, "DROP TABLE books");
-}
-
-async function main(database) {
-  await databaseWithNoExistingError(database);
-  await databaseWithExistingError(database);
-}
-
-main(database);
+await runWithPromise(database, "DROP TABLE books");
