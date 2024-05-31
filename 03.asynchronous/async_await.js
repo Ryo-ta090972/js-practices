e324909cdb730b159f9b3113fa202894feefe6a9
ryo <ta090972@icloud.com>
2024-05-31 19:50:28 +0900

    asyncとpromiseのファイル間で共通するコードを共通関数として定義した

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -2,56 +2,43 @@
 
 import sqlite3 from "sqlite3";
 import {
-  runWithPromise,
-  allWithPromise,
-} from "./sqlite_functions_with_promise.js";
+  createBooksTable,
+  selectBooksTable,
+  selectGamesTable,
+  dropBooksTable,
+  insertTitle,
+  insertContent,
+} from "./database_operation_functions.js";
 import { handleDatabaseError } from "./handle_error.js";
 
 const database = new sqlite3.Database(":memory:");
 
 // エラー無し
-await runWithPromise(
-  database,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
+await createBooksTable(database);
 
-const statement = await runWithPromise(
-  database,
-  "INSERT INTO books (title) VALUES (?)",
-  ["プロを目指す人のためのRuby入門"],
-);
+const statement = await insertTitle(database);
 console.log("追加したID:", statement.lastID);
 
-const rowsOfBooksTable = await allWithPromise(database, "SELECT * FROM books");
+const rowsOfBooksTable = await selectBooksTable(database);
 console.log("取得したデータ：", rowsOfBooksTable);
 
-await runWithPromise(database, "DROP TABLE books");
+await dropBooksTable(database);
 
 // エラーあり
-await runWithPromise(
-  database,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-);
+await createBooksTable(database);
 
 try {
-  const statement = await runWithPromise(
-    database,
-    "INSERT INTO books (content) VALUES (?)",
-    ["Rubyを知れば、Railsはもっと楽しくなる"],
-  );
+  const statement = await insertContent(database);
   console.log("追加したID:", statement.lastID);
 } catch (error) {
   handleDatabaseError(error);
 }
 
 try {
-  const rowsOfGamesTable = await allWithPromise(
-    database,
-    "SELECT * FROM games",
-  );
+  const rowsOfGamesTable = await selectGamesTable(database);
   console.log("取得したデータ：", rowsOfGamesTable);
 } catch (error) {
   handleDatabaseError(error);
 }
 
-await runWithPromise(database, "DROP TABLE books");
+await dropBooksTable(database);
--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -3,57 +3,47 @@
 import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 import {
-  runWithPromise,
-  allWithPromise,
-} from "./sqlite_functions_with_promise.js";
+  createBooksTable,
+  selectBooksTable,
+  selectGamesTable,
+  dropBooksTable,
+  insertTitle,
+  insertContent,
+} from "./database_operation_functions.js";
 import { handleDatabaseError } from "./handle_error.js";
 
 const database = new sqlite3.Database(":memory:");
 
 // エラー無し
-runWithPromise(
-  database,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-)
-  .then(() =>
-    runWithPromise(database, "INSERT INTO books (title) VALUES (?)", [
-      "プロを目指す人のためのRuby入門",
-    ]),
-  )
+createBooksTable(database)
+  .then(() => insertTitle(database))
   .then((statement) => {
     console.log("追加したID:", statement.lastID);
-    return allWithPromise(database, "SELECT * FROM books");
+    return selectBooksTable(database);
   })
   .then((rowsOfBooksTable) => {
     console.log("取得したデータ：", rowsOfBooksTable);
-    runWithPromise(database, "DROP TABLE books");
+    dropBooksTable(database);
   });
 
 await timers.setTimeout(100);
 
 // エラーあり
-runWithPromise(
-  database,
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-)
-  .then(() =>
-    runWithPromise(database, "INSERT INTO books (content) VALUES (?)", [
-      "Rubyを知れば、Railsはもっと楽しくなる",
-    ]),
-  )
+createBooksTable(database)
+  .then(() => insertContent(database))
   .then((statement) => {
     console.log("追加したID:", statement.lastID);
-    return allWithPromise(database, "SELECT * FROM games");
+    return selectGamesTable(database);
   })
   .catch((error) => {
     handleDatabaseError(error);
-    return allWithPromise(database, "SELECT * FROM games");
+    return selectGamesTable(database);
   })
   .then((rowsOfGamesTable) => {
     console.log("取得したデータ：", rowsOfGamesTable);
-    runWithPromise(database, "DROP TABLE books");
+    dropBooksTable(database);
   })
   .catch((error) => {
     handleDatabaseError(error);
-    runWithPromise(database, "DROP TABLE books");
+    dropBooksTable(database);
   });
