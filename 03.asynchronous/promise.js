dd05c7d3a56f07fea1e8c8f33dd147c1711dd3ff
ryo <ta090972@icloud.com>
2024-06-02 22:44:33 +0900

    共通コードをなくすためthen~catchを入り子にした

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -3,47 +3,51 @@
 import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 import {
-  createBooksTable,
-  selectBooksTable,
-  selectGamesTable,
-  dropBooksTable,
-  insertTitle,
-  insertContent,
-} from "./database_operation_functions.js";
+  runWithPromise,
+  allWithPromise,
+} from "./sqlite_functions_with_promise.js";
 import { handleDatabaseError } from "./handle_error.js";
 
 const database = new sqlite3.Database(":memory:");
 
 // エラー無し
-createBooksTable(database)
-  .then(() => insertTitle(database))
+runWithPromise(
+  database,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() =>
+    runWithPromise(database, "INSERT INTO books (title) VALUES (?)", [
+      "プロを目指す人のためのRuby入門",
+    ]),
+  )
   .then((statement) => {
     console.log("追加したID:", statement.lastID);
-    return selectBooksTable(database);
+    return allWithPromise(database, "SELECT * FROM books");
   })
   .then((rowsOfBooksTable) => {
     console.log("取得したデータ：", rowsOfBooksTable);
-    dropBooksTable(database);
+    runWithPromise(database, "DROP TABLE books");
   });
 
 await timers.setTimeout(100);
 
 // エラーあり
-createBooksTable(database)
-  .then(() => insertContent(database))
-  .then((statement) => {
-    console.log("追加したID:", statement.lastID);
-    return selectGamesTable(database);
-  })
-  .catch((error) => {
-    handleDatabaseError(error);
-    return selectGamesTable(database);
-  })
-  .then((rowsOfGamesTable) => {
-    console.log("取得したデータ：", rowsOfGamesTable);
-    dropBooksTable(database);
-  })
-  .catch((error) => {
-    handleDatabaseError(error);
-    dropBooksTable(database);
-  });
+runWithPromise(
+  database,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() =>
+    runWithPromise(database, "INSERT INTO books (content) VALUES (?)", [
+      "Rubyを知れば、Railsはもっと楽しくなる",
+    ])
+      .then((statement) => console.log("追加したID:", statement.lastID))
+      .catch((error) => handleDatabaseError(error)),
+  )
+  .then(() =>
+    allWithPromise(database, "SELECT * FROM games")
+      .then((rowsOfGamesTable) =>
+        console.log("取得したデータ：", rowsOfGamesTable),
+      )
+      .catch((error) => handleDatabaseError(error)),
+  )
+  .then(() => runWithPromise(database, "DROP TABLE books"));
