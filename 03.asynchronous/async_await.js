5742bbc023a9ebc16b9f661df79f77228cc4c974
ryo <ta090972@icloud.com>
2024-06-02 22:45:15 +0900

    共通関数削除に伴う修正

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -2,43 +2,56 @@
 
 import sqlite3 from "sqlite3";
 import {
-  createBooksTable,
-  selectBooksTable,
-  selectGamesTable,
-  dropBooksTable,
-  insertTitle,
-  insertContent,
-} from "./database_operation_functions.js";
+  runWithPromise,
+  allWithPromise,
+} from "./sqlite_functions_with_promise.js";
 import { handleDatabaseError } from "./handle_error.js";
 
 const database = new sqlite3.Database(":memory:");
 
 // エラー無し
-await createBooksTable(database);
+await runWithPromise(
+  database,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-const statement = await insertTitle(database);
+const statement = await runWithPromise(
+  database,
+  "INSERT INTO books (title) VALUES (?)",
+  ["プロを目指す人のためのRuby入門"],
+);
 console.log("追加したID:", statement.lastID);
 
-const rowsOfBooksTable = await selectBooksTable(database);
+const rowsOfBooksTable = await allWithPromise(database, "SELECT * FROM books");
 console.log("取得したデータ：", rowsOfBooksTable);
 
-await dropBooksTable(database);
+await runWithPromise(database, "DROP TABLE books");
 
 // エラーあり
-await createBooksTable(database);
+await runWithPromise(
+  database,
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
 try {
-  const statement = await insertContent(database);
+  const statement = await runWithPromise(
+    database,
+    "INSERT INTO books (content) VALUES (?)",
+    ["Rubyを知れば、Railsはもっと楽しくなる"],
+  );
   console.log("追加したID:", statement.lastID);
 } catch (error) {
   handleDatabaseError(error);
 }
 
 try {
-  const rowsOfGamesTable = await selectGamesTable(database);
+  const rowsOfGamesTable = await allWithPromise(
+    database,
+    "SELECT * FROM games",
+  );
   console.log("取得したデータ：", rowsOfGamesTable);
 } catch (error) {
   handleDatabaseError(error);
 }
 
-await dropBooksTable(database);
+await runWithPromise(database, "DROP TABLE books");
