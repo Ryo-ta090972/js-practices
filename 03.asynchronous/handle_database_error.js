7b1abccb3a4d14d262120c1df1c3fb2fe5345762
ryo <ta090972@icloud.com>
2024-06-04 17:07:56 +0900

    エラーオブジェクト以外のerrorもthrowするように変更した

--- 03.asynchronous/handle_database_error.js
+++ 03.asynchronous/handle_database_error.js
@@ -1,7 +1,7 @@
 export function handleDatabaseError(error) {
   if (error instanceof Error && error.code === "SQLITE_ERROR") {
     console.error("捕捉したいエラー:", error.message);
-  } else if (error instanceof Error) {
+  } else {
     throw error;
   }
 }
