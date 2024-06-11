20b55cd6e5288dea3efab5d0969fef39cf74d73a
ryo <ta090972@icloud.com>
2024-06-11 10:38:21 +0900

    コマンドライン引数を取り扱うクラスを作成した

---
+++ 04.class/command_line.js
@@ -0,0 +1,31 @@
+import { Command } from "commander";
+
+export class CommandLine {
+  #program;
+
+  constructor() {
+    this.#program = this.#parseProgram();
+  }
+
+  get options() {
+    return this.#program.opts();
+  }
+
+  isOption() {
+    const number = Object.keys(this.options).length;
+
+    if (number === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  #parseProgram() {
+    return new Command()
+      .option("-l, --list", "データベース内のメモの先頭行を表示する")
+      .option("-r, --read", "選んだメモの全行を表示する")
+      .option("-d, --delete", "選んだメモを削除する")
+      .parse();
+  }
+}
