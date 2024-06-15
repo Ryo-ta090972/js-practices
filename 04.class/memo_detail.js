27c8021ab9d3e42ec24a54aed69af2ee688180c2
ryo <ta090972@icloud.com>
2024-06-15 15:03:02 +0900

    先頭行が改行で始まる場合、firstRowが返す値を「NO TITLE」にした。

--- 04.class/memo_detail.js
+++ 04.class/memo_detail.js
@@ -26,8 +26,10 @@ export class MemoDetail {
   #extractFirstRow() {
     const indexOfFirstRow = this.#content.indexOf("\n");
 
-    if (indexOfFirstRow >= 0) {
+    if (indexOfFirstRow >= 1) {
       return this.#content.substring(0, indexOfFirstRow);
+    } else if (this.#content === "" || indexOfFirstRow === 0) {
+      return "NO TITLE";
     } else {
       return this.#content;
     }
